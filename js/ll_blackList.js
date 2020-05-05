aaec46dba67177c973bbbf135a9fde7f3ebde474
linlu <1318486974@qq.com>
2020-05-05 10:20:11 +0800

    Update ll_blackList.js

--- js/ll_blackList.js
+++ js/ll_blackList.js
@@ -2,6 +2,8 @@ mui.init();
 mui.plusReady(function() {
 	var thisWebview = plus.webview.currentWebview();
 	thisWebview.addEventListener("show", function() {
+		//发送请求到后端，返回黑名单
+		requestBlackList();
 		//从缓存中获取黑名单列表，并且渲染到页面
 		renderBlackListPage();
 	});
@@ -49,65 +51,12 @@ mui.plusReady(function() {
 	})
 	
 	
-	
-	/*(function($) {
-		$('#ulBlackList').on('tap', '.mui-btn', function(event) {
-			var elem = this;
-			var li = elem.parentNode.parentNode;
-			var con=mui.confirm('确认将该联系人从黑名单移出？', btnArray, function(e) {
-				if (e.index == 0) {
-					var addedId=li.value;
-					var userId=app.getUserGlobalInfo().userId;
-					li.parentNode.removeChild(li);
-					//alert("删除成功");
-					//传值给详情页面，通知加载新数据
-					//mui.fire(plus.webview.getWebviewById(ll_personalCenter.html),'refreshBlackList',{userId:userId,addedId:addedId});
-					//打开新闻详情
-					//mui.openWindow({
-					//	id:'ll_personalCenter.html',
-					 //   url:'ll_personalCenter.html'
-					 //});
-				} else {
-					setTimeout(function() {
-						$.swipeoutClose(li);
-					}, 0);
-				}
-			});
-		});
-		var btnArray = ['确认', '取消'];
-	})(mui);*/
-	
 })
+
 //从缓存中获取用户黑名单，并且渲染到页面
 function renderBlackListPage() {
-	//获取黑名单列表
+	//从缓存获取黑名单列表
 	var blackList = app.getBlackList();
-	//发送请求返回黑名单列表
-	if(blackList==null){
-		mui.ajax('http://server-name/login.php', {
-			data: {
-				userId: app.getUserGlobalInfo().userId,
-			},
-			dataType: 'json', //服务器返回json格式数据
-			type: 'post', //HTTP请求类型
-			timeout: 10000, //超时时间设置为10秒；
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			success: function(data) {
-				//服务器返回响应，根据响应结果，分析是否修改成功；
-				if (data.status == 200) {
-					//刷新用户黑名单信息
-					var blackListJson = data.data;
-					app.setblackList(blackListJson);
-				}
-			},
-			error: function(xhr, type, errorThrown) {
-				//异常处理；
-				console.log(type);
-			}
-		});
-	}
 	//显示黑名单列表
 	var ulBlackList = document.getElementById("ulBlackList");
 	if (blackList != null && blackList.length > 0) {
@@ -120,6 +69,7 @@ function renderBlackListPage() {
 		ulBlackList.innerHTML = "";
 	}
 }
+
 function renderUser(user) { //设置用户列表的单个列表项
 	var html = "";
 	html = '<li class="mui-table-view-cell" '+'value="'+user.userId+ '">'+
@@ -130,4 +80,31 @@ function renderUser(user) { //设置用户列表的单个列表项
 		'</div>' +
 		'</li>';
 	return html;
+}
+
+//向后端发送请求获取黑名单
+function requestBlackList(){
+	mui.ajax('http://server-name/login.php', {
+		data: {
+			userId: app.getUserGlobalInfo().userId,
+		},
+		dataType: 'json', //服务器返回json格式数据
+		type: 'post', //HTTP请求类型
+		timeout: 10000, //超时时间设置为10秒；
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		success: function(data) {
+			//服务器返回响应，根据响应结果，分析是否修改成功；
+			if (data.status == 200) {
+				//刷新用户黑名单信息
+				var blackListJson = data.data;
+				app.setblackList(blackListJson);
+			}
+		},
+		error: function(xhr, type, errorThrown) {
+			//异常处理；
+			console.log(type);
+		}
+	});
 }
