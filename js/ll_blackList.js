5040f24d8473125b62b41a62477a09cbe00cd359
linlu <1318486974@qq.com>
2020-05-05 10:20:59 +0800

    Update ll_blackList.js

--- js/ll_blackList.js
+++ js/ll_blackList.js
@@ -7,50 +7,49 @@ mui.plusReady(function() {
 		//从缓存中获取黑名单列表，并且渲染到页面
 		renderBlackListPage();
 	});
-	
-	document.getElementById("delete").addEventListener('tap',function(){
-		var statu = confirm("确认将该联系人从黑名单移出吗?");//在js里面写confirm，在页面中弹出提示信息。
-		if(!statu)//如果点击的是取消
+
+	document.getElementById("delete").addEventListener('tap', function() {
+		var statu = confirm("确认将该联系人从黑名单移出吗?"); //在js里面写confirm，在页面中弹出提示信息。
+		if (!statu) //如果点击的是取消
 		{
-		    return false;//返回页面
+			return false; //返回页面
+		} else { //如果点击确定，就继续执行下面的操作
+			var elem = this;
+			var li = elem.parentNode.parentNode;
+			var addedId = li.value;
+			var userId = app.getUserGlobalInfo().userId;
+			//向服务器发送请求删除该黑名单用户
+			mui.ajax('http://server-name/login.php', {
+				data: {
+					userId: userId,
+					addedId: addedId
+				},
+				dataType: 'json', //服务器返回json格式数据
+				type: 'post', //HTTP请求类型
+				timeout: 10000, //超时时间设置为10秒；
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				success: function(data) {
+					//服务器返回响应，根据响应结果，分析是否删除成功；
+					if (data.status == 200) {
+						//删除黑名单缓存
+						plus.storage.removeItem("blackList");
+						// 页面跳转到默认首页（后续需更改
+						mui.openWindow("ll_personalCenter.html", "ll_personalCenter.html");
+					}
+				},
+				error: function(xhr, type, errorThrown) {
+					//异常处理；
+					console.log(type);
+				}
+			});
+			alert("保存成功");
+			window.open('ll_personalCenter.html');
 		}
-		else{//如果点击确定，就继续执行下面的操作
-		    var elem = this;
-		    var li = elem.parentNode.parentNode;
-		    var addedId=li.value;
-		    var userId=app.getUserGlobalInfo().userId;
-		    //向服务器发送请求删除该黑名单用户
-		    mui.ajax('http://server-name/login.php', {
-		    	data: {
-		    		userId: userId,
-		    		addedId: addedId
-		    	},
-		    	dataType: 'json', //服务器返回json格式数据
-		    	type: 'post', //HTTP请求类型
-		    	timeout: 10000, //超时时间设置为10秒；
-		    	headers: {
-		    		'Content-Type': 'application/json'
-		    	},
-		    	success: function(data) {
-		    		//服务器返回响应，根据响应结果，分析是否删除成功；
-		    		if (data.status == 200) {
-		    			//删除黑名单缓存
-		    			plus.storage.removeItem("blackList");
-		    			// 页面跳转到默认首页（后续需更改
-		    			mui.openWindow("ll_personalCenter.html", "ll_personalCenter.html");
-		    		}
-		    	},
-		    	error: function(xhr, type, errorThrown) {
-		    		//异常处理；
-		    		console.log(type);
-		    	}
-		    });
-		    alert("保存成功");
-		    window.open('ll_personalCenter.html');
-		    }
 	})
-	
-	
+
+
 })
 
 //从缓存中获取用户黑名单，并且渲染到页面
@@ -72,7 +71,7 @@ function renderBlackListPage() {
 
 function renderUser(user) { //设置用户列表的单个列表项
 	var html = "";
-	html = '<li class="mui-table-view-cell" '+'value="'+user.userId+ '">'+
+	html = '<li class="mui-table-view-cell" ' + 'value="' + user.userId + '">' +
 		'<div class="mui-slider-right mui-disabled">' +
 		'<a class="mui-btn mui-btn-red">删除</a>' +
 		'</div>' +
@@ -83,7 +82,7 @@ function renderUser(user) { //设置用户列表的单个列表项
 }
 
 //向后端发送请求获取黑名单
-function requestBlackList(){
+function requestBlackList() {
 	mui.ajax('http://server-name/login.php', {
 		data: {
 			userId: app.getUserGlobalInfo().userId,
