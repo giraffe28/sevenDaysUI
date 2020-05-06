1e006f4d9acc6272f0fae4401f29c1c69e357250
linlu <1318486974@qq.com>
2020-05-06 09:49:06 +0800

    about memory

--- js/ll_addMemory.js
+++ js/ll_addMemory.js
@@ -1,17 +1,28 @@
 mui.init();
-/*document.getElementById("savebtn").addEventListener('tap',function(){
-	mui.ajax(app.serverUrl + "", {
-		data: {
-			userId:'';
-			addedId:'';
-		},
-		dataType: 'json', //服务器返回json格式数据
-		type: 'post', //HTTP请求类型
-		timeout: 10000, //超时时间设置为10秒；
-		success: function(data) {
-			console.log(JSON.stringify(data));
-		},
-		error:function(xhr,type,errorThrown){
-			console.log(type);
-		}
-});*/
+mui.plusReady(function () {
+	var memoryTitle=document.getElementById("title").value;
+	var memoryContent=document.getElementById("content").value;
+    document.getElementById("save").addEventListener('tap',function(){
+		mui.ajax(app.serverUrl+"...",{
+			data:{
+				userId:app.getUserGlobalInfo().userId,
+				memoryTitle:memoryTitle,
+				memoryContent:memoryContent,
+			},
+			dataType:'json',//服务器返回json格式数据
+			type:'post',//HTTP请求类型
+			timeout:10000,//超时时间设置为10秒；
+			success:function(data){
+				console.log(JSON.stringify(data));
+				if(data.status==200){
+					mui.toast('保存成功！');
+					app.setMemory("memory");
+					mui.openWindow("ll_crossingMemory.html", "ll_crossingMemory.html");
+				}
+			},
+			error:function(xhr,type,errorThrown){
+				
+			}
+		});
+	})
+})
