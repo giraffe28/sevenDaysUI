bf21a010e469c6cb508e9632fc769ca36d9f20ef
ANDTENET <crb19990618@163.com>
2020-05-24 21:27:58 +0800

    Update crb_details.js

--- js/crb_details.js
+++ js/crb_details.js
@@ -256,6 +256,54 @@ mui.plusReady(function () {
 		
 	});
 	
+	var send=document.getElementById("send-btn");
+	send.addEventListener('tap',function(){
+		var content=document.getElementById('comment-text').value;
+		if(content==''){
+			mui.toast('内容不能为空');
+			return false; 
+		}
+		else if(content.length>50){
+		   mui.toast('评论不得超过50个字');
+		   return false; 
+	   }
+	   else{
+	   var myDate = new Date();
+	   mui.ajax(app.serverUrl+"/corner/talk", {
+	   	data: {
+			user:{
+				userId:user.userId
+			},
+	   		commentcontent:content
+	   	},
+	   	dataType: 'json', //服务器返回json格式数据
+	   	type: 'post', //HTTP请求类型
+	   	timeout: 10000, //超时时间设置为10秒；
+	   	headers: {
+	   		'Content-Type': 'application/json;charset:utf-8'
+	   	},
+	   	success: function(data) {
+	   		//服务器返回响应，根据响应结果，分析是否成功发送动态；
+	   		if (data.status == 200) {
+	   			//显示成功信息
+	   			//mui.toast("发送评论成功");
+				console.log(data.data);
+				var chatWebview = plus.webview.getWebviewById("crb_details.html");
+				chatWebview.evalJS("pulldownRefresh()");
+				mui.back();
+	   		}
+	   		else{
+	   			app.showToast(data.msg, "error");
+	   		}
+	   	},
+	   	error: function(xhr, type, errorThrown) {
+	   		//异常处理；
+	   		console.log(type);
+	   	}
+	   });
+	   }
+	});	
+	
 	
 	//me=app.getUserGlobalInfo();//获取用户信息	
 	//标题改为朋友的昵称
@@ -275,44 +323,3 @@ mui.plusReady(function () {
 	
 	
 })
-
-/*mui.plusReady(function(){
-	//点击发送评论按钮
-	var send=document.getElementById("send");
-	send.addEventListener('tap',function(){
-		var comment=document.getElementById("comment").value;
-		if(comment.length>20){
-			mui.toast("评论不得超过20个字");
-			return false; 
-		}
-		var user = app.getUserGlobalInfo();
-		var myDate = new Date();
-		mui.ajax('……', {
-		data: {
-			userId:user.userId,
-			comment:'comment',
-			comment_time:myDate.toLocaleString()
-		},
-		dataType: 'json', //服务器返回json格式数据
-		type: 'post', //HTTP请求类型
-		timeout: 10000, //超时时间设置为10秒；
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		success: function(data) {
-			//服务器返回响应，根据响应结果，分析是否成功发送评论；
-			if (data.status == 200) {
-				//显示成功信息
-				mui.toast("评论成功");
-			}
-			else{
-				app.showToast(data.msg, "error");
-			}
-		},
-			error: function(xhr, type, errorThrown) {
-			//异常处理；
-			console.log(type);
-		}
-	});
-})
-})*/
