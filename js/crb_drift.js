fb951e1848772348a6b01fbdcf89e9229c393051
17lhf <60611483+17lhf@users.noreply.github.com>
2020-06-06 21:55:42 +0800

    优化搜索食堂的及时清空内容，增加给陌生人发送聊天时的聊天页面跳转

--- js/crb_drift.js
+++ js/crb_drift.js
@@ -1,10 +1,9 @@
 mui.init();
 mui.plusReady(function(){
-	//var userInfo=app.getUserGlobalInfo();
 	
 	var get=document.getElementById("get");
 	var throwout=document.getElementById("throwout");
-    var mine=document.getElementById("mine");
+    //var mine=document.getElementById("mine");
 	
 	get.addEventListener('tap',function(){
 		mui.openWindow({
@@ -19,11 +18,12 @@ mui.plusReady(function(){
 			id:'crb_throwbottle.html'
 		})
 	});
-	
+	/*
 	mine.addEventListener('tap',function(){
 		mui.openWindow({
 			url:'../html/crb_mybottle.html',
 			id:'crb_mybottle.html'
 		})
-	});
+	});*/
+	
 })
